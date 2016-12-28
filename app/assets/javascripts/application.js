1883529b451f4df0dffd861bc6ab42f557e3d6c5
Junaid Paracha <Junaid.p.1983@gamil.com>
2016-12-28 01:04:43 -0600

    SkyHouse

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,16 +19,11 @@
 $('.carousel').carousel({
   interval: 2000
 });
-$( "data-href" ).mouseover(function() {
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-})
-$('link_to').click(function() {
-    interval: 2000;
-    location.reload();
-});
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
